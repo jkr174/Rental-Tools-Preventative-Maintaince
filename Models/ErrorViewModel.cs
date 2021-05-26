49da8cb12d84a2d6f2fe13ec823a87359eeae2e3
jkr174 <romo17410@gmail.com>
2021-05-25 20:02:11 -0500

    Update
    Initial MVC Project Creation

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RentalToolsPreventativeMaintaince.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
